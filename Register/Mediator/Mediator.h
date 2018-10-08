030fc52b18a16faa45cfdb86d87f2657723456ba
月亮酱 <42065449+Cirno0@users.noreply.github.com>
2018-10-08 12:42:21 +0800

    Revert "updata"
    This reverts commit 36424e508186003e835e6255f571558ff818704a.

--- Register/Mediator/Mediator.h
+++ Register/Mediator/Mediator.h
@@ -1,23 +1,18 @@
 #pragma once
 
 class CBaseControl;
-class Mediator:public CWnd
+
+class Mediator
 {
-	DECLARE_DYNAMIC(Mediator)
 public:
 	Mediator();
 	~Mediator();
 	void InitControl(CWnd * pParentWnd);
 	void ShowControl(Graphics* &);
 	bool GetTask();
-	BOOL Create(Rect rect,CWnd * pParentWnd,UINT nID);
-protected:
-	DECLARE_MESSAGE_MAP()
 private:
 	bool task_flag;
 	std::vector<CBaseControl *> vec_control;
 	
-public:
-	afx_msg void OnPaint();
 };
 
